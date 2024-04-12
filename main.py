783c88d4ecd272992f878eba8ddb047e52b80fdb
Tyler-Cav <tyler.cavanaugh24@gmail.com>
2024-04-12 14:41:04 -0400

    updated to track character count within text

--- main.py
+++ main.py
@@ -1,1 +1,24 @@
-print("Hello World")
+def main (): 
+            
+    bookCharacterCounts =	{}
+
+    with open('./books/frankenstein.txt') as f:
+        file_contents = f.read()
+        # print(file_contents)
+        def howManyWords(): 
+            words = file_contents.split()
+            print(len(words))
+        howManyWords()
+        
+        # Looping over dictionary to check if the character key already exists
+        lowerCase = file_contents.lower()
+        for z in lowerCase:
+            if bookCharacterCounts.get(z) == None:
+                bookCharacterCounts[f'{z}'] = 1
+            else: 
+                bookCharacterCounts[z] += 1
+        print(bookCharacterCounts)
+                
+main()
+
+  
